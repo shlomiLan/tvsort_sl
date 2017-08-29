27109c39f584d430b5080a454c2f5eab636828ff
Shlomi Lanton <shlomilanton@gmail.com>
2017-08-29 07:31:46 +0300

    Changes in setup and readme

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 from __future__ import unicode_literals
 
-from setuptools import setup
+from distutils.core import setup
 
 
 setup(
     name='tvsort_sl',
     packages=['tvsort_sl'],
-    version='0.8',
+    version='0.8.1.1',
     description='Sort movies and TV-shows files',
     long_description=open('README.rst').read(),
     author='Shlomi Lanton',
