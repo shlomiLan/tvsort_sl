9a9311e493e308280e83001f9379c24a0b8b3f53
shlomiLan <shlomilanton@getwizwer.com>
2017-12-01 21:46:36 +0200

    overwrite 'fcntl' module to fix issues in Windows evn

---
+++ tvsort_sl/fcntl.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
+
+def fcntl(fd, op, arg=0):
+    return 0
+
+
+def ioctl(fd, op, arg=0, mutable_flag=True):
+    if mutable_flag:
+        return 0
+    else:
+        return ""
+
+
+def flock(fd, op):
+    return
+
+
+def lockf(fd, operation, length=0, start=0, whence=0):
+    return
