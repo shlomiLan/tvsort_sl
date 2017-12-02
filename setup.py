18a729ef45474457bf0995a97d226869057ea821
shlomiLan <shlomilanton@getwizwer.com>
2017-12-02 20:47:59 +0200

    Update setup file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 setup(
     name='tvsort_sl',
     packages=['tvsort_sl'],
-    version='0.8.1.1',
+    version='1.0',
     description='Sort movies and TV-shows files',
     long_description=open('README.rst').read(),
     author='Shlomi Lanton',
