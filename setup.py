c402d057496d27252dc2e327a14954a5d6b03de9
shlomiLan <shlomilanton@gmail.com>
2019-08-02 18:39:59 +0300

    Fix readme file in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['tvsort_sl'],
     version='1.0.2',
     description='Sort movies and TV-shows files',
-    long_description=open('README.rst').read(),
+    long_description=open('readme.md').read(),
     author='Shlomi Lanton',
     author_email='shlomilanton@gmail.com',
     url='https://github.com/shlomiLan/tvsort_sl',
