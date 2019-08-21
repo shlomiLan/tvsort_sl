35c1a5ecfcbaa8bf098c04b478bdeb0f43842bb5
shlomiLan <shlomilanton@gmail.com>
2019-08-22 00:39:08 +0300

    Undo bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='tvsort_sl',
     packages=['tvsort_sl'],
-    version='1.1.3',
+    version='1.1.6',
     description='Sort movies and TV-shows files',
     long_description=open('readme.md').read(),
     long_description_content_type='text/markdown',
