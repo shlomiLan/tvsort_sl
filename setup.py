32e0d03e6b108defa875a62456b557241275531e
Shlomi Lanton <shlomiLan@users.noreply.github.com>
2020-06-03 17:10:16 +0300

    Update version, file: setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='tvsort_sl',
     packages=['tvsort_sl'],
-    version='1.1.18',
+    version='1.1.19',
     description='Sort movies and TV-shows files',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
