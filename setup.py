eff2dcc79c9e1f2d8beef4f600917f90f0aabe47
shlomiLan <shlomilanton@gmail.com>
2024-10-24 17:30:08 +0300

    Fix issue

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
 
+def long_description():
+    with open('README.md', 'r', encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
     name='tvsort_sl',
     packages=['tvsort_sl'],
     version='2.0.0',
     description='Sort movies and TV-shows files',
-    long_description=open('README.md').read(),
+    long_description=long_description(),
     long_description_content_type='text/markdown',
     author='Shlomi Lanton',
     author_email='shlomilanton@gmail.com',
