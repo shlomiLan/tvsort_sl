49289a46f3d4aa5361956f3d31ffdc5f6cd7f732
shlomiLan <shlomilanton@gmail.com>
2024-10-24 17:33:34 +0300

    comma to to dubule

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from setuptools import setup
 
 
 def long_description():
-    with open('README.md', 'r', encoding='utf-8') as f:
+    with open("README.md", "r", encoding="utf-8") as f:
         return f.read()
 
 
 setup(
-    name='tvsort_sl',
-    packages=['tvsort_sl'],
-    version='2.0.0',
-    description='Sort movies and TV-shows files',
+    name="tvsort_sl",
+    packages=["tvsort_sl"],
+    version="2.0.0",
+    description="Sort movies and TV-shows files",
     long_description=long_description(),
-    long_description_content_type='text/markdown',
-    author='Shlomi Lanton',
-    author_email='shlomilanton@gmail.com',
-    url='https://github.com/shlomiLan/tvsort_sl',
-    download_url='https://github.com/shlomiLan/tvsort_sl/archive/0.1.zip',
-    keywords=['sort', 'tv', 'show', 'movie', 'KODI', 'XBM1C'],
+    long_description_content_type="text/markdown",
+    author="Shlomi Lanton",
+    author_email="shlomilanton@gmail.com",
+    url="https://github.com/shlomiLan/tvsort_sl",
+    download_url="https://github.com/shlomiLan/tvsort_sl/archive/0.1.zip",
+    keywords=["sort", "tv", "show", "movie", "KODI", "XBM1C"],
     classifiers=[],
-    setup_requires=['wheel']
+    setup_requires=["wheel"]
 )
