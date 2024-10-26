c08868d50493a467f2704b3ec7500c3e93213ce2
shlomiLan <shlomilanton@gmail.com>
2024-10-26 15:09:49 +0300

    dummy commit

--- tvsort_sl/utils.py
+++ tvsort_sl/utils.py
@@ -137,7 +137,7 @@ def add_missing_country(video: dict, show_name: str) -> None:
 def create_file(file_path: str) -> List[Tuple[str, str]]:
     with open(file_path, 'w', encoding="utf-8"):
         return [('info', f'File was created, in: {file_path}')]
-
+\
 
 def delete_file(file_path: str) -> List[Tuple[str, str]]:
     try:
